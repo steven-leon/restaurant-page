7bde80c1ce7b57378f360e03761d36bdb258ec2f
steven leon <stevenleon145@gmail.com>
2023-12-13 00:25:49 -0500

    github pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'main.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+// };
+
+
+ const path = require('path');
+
+ module.exports = {
+   entry: './src/index.js',
+   output: {
     path: path.resolve(__dirname, 'dist'),
-  },
-};
+    filename: 'bundle.js',
+    publicPath: '/restaurant-page/' // Aquí cambias el valor
+    },
+ };
