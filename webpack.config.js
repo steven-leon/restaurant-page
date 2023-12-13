debdc4f68bcb1a95f30f35de0fe721126dfa1fd0
steven leon <stevenleon145@gmail.com>
2023-12-13 01:52:21 -0500

    otro intento

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,13 @@
 // };
 
 
- const path = require('path');
+const path = require("path");
 
- module.exports = {
-   entry: './src/index.js',
-   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/restaurant-page/' // Aquí cambias el valor
-    },
- };
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
